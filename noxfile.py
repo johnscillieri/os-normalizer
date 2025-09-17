7351c696fa99170e7edac6aa2c166975319da4f2
John Scillieri <5930913+johnscillieri@users.noreply.github.com>
2025-09-17 06:28:59 -0400

    Update to add 3.14 and 3.14t to test matrix

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ nox.options.default_venv_backend = "uv"
 
 
 @session(
-    python=["3.11", "3.12", "3.13"],
+    python=["3.11", "3.12", "3.13", "3.14", "3.14t"],
 )
 def tests(session: nox.Session) -> None:
     """Run test suite with pytest."""
